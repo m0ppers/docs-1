c6709edae22b8bd11c4e42b2dbaac51182b781fc
Andreas Streichardt <andreas@mop.koeln>
2019-04-30 16:31:51 +0200

    👷 netlify test

---
+++ netlify.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+set -e
+
+jekyll build
+rm -rf htmltest
+mkdir -p htmltest
+# our baseUrl is /docs so we need to create that structure for htmltest
+# otherwise it would fail to find absolute links like /docs/3.5
+cp -a _site htmltest/docs
+
+cd /tmp && \
+  wget https://github.com/wjdp/htmltest/releases/download/v0.10.1/htmltest_0.10.1_linux_amd64.tar.gz && \
+  tar xvzf htmltest_0.10.1_linux_amd64.tar.gz && \
+  mv htmltest /usr/local/bin
+htmltest -s
+
