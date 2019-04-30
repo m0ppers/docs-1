5031dbfcb2e1dcca0979e27a36d74ba3dc858db2
Andreas Streichardt <andreas@mop.koeln>
2019-04-30 16:57:57 +0200

    💚 test

--- netlify.sh
+++ netlify.sh
@@ -11,7 +11,11 @@ cp -a _site htmltest/docs
 
 cd /tmp
 wget https://github.com/wjdp/htmltest/releases/download/v0.10.1/htmltest_0.10.1_linux_amd64.tar.gz
+echo "TESTTEESTEETST"
+sleep 10
 tar xvzf htmltest_0.10.1_linux_amd64.tar.gz
+echo "TESTTEESTEETST2"
+sleep 10
 mv htmltest /usr/local/bin
 cd -
 htmltest -s
