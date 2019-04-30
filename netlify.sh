f97bb97a5f59666f50382d776740df0ed0655662
Andreas Streichardt <andreas@mop.koeln>
2019-04-30 16:53:05 +0200

    🐛 test

--- netlify.sh
+++ netlify.sh
@@ -9,9 +9,10 @@ mkdir -p htmltest
 # otherwise it would fail to find absolute links like /docs/3.5
 cp -a _site htmltest/docs
 
-(cd /tmp && \
-  wget https://github.com/wjdp/htmltest/releases/download/v0.10.1/htmltest_0.10.1_linux_amd64.tar.gz && \
-  tar xvzf htmltest_0.10.1_linux_amd64.tar.gz && \
-  mv htmltest /usr/local/bin)
+cd /tmp
+wget https://github.com/wjdp/htmltest/releases/download/v0.10.1/htmltest_0.10.1_linux_amd64.tar.gz
+tar xvzf htmltest_0.10.1_linux_amd64.tar.gz
+mv htmltest /usr/local/bin
+cd -
 htmltest -s
 
